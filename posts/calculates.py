2b7a8e631ae920a174e729d6f5c7c775822e7713
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-08-05 15:32:54 +0900

    fix : calculates.py 수정

--- posts/calculates.py
+++ posts/calculates.py
@@ -16,5 +16,5 @@ def calculate(meal, amount,  result, c_result):
     result[11] += meal[i].tryptophan * amount[i] / 100
     result[12] += meal[i].dha_epa * amount[i] / 100
     # 카테고리 추가
-    c_result |= meal[i].category.values_list()
+    # c_result |= meal[i].category.values_list() # 임시 보류
   return result, c_result
